95f9c0e32ecc555751e66f801cd8cb0be96d0ef6
abusayeds <abusayedstudent855@gmail.com>
2026-04-07 09:01:24 +0600

    service and controller create .

---
+++ src/modules/make_modules/terms/terms.route.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { TermsController } from "./terms.controller";
+import { authMiddleware } from "../../../middlewares/auth";
+import { role } from "../../../utils/role";
+
+const router = express.Router();
+
+router.post("/", authMiddleware(role.admin), TermsController.createTerms);
+router.get("/", authMiddleware(role.user), TermsController.getTerms);
+router.patch("/", authMiddleware(role.admin), TermsController.updateTerms);
+router.delete("/", authMiddleware(role.admin), TermsController.deleteTerms);
+
+export const TermsRoutes = router;
