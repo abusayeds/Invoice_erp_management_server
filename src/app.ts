794fbf5577db3483f365c56a2f9bfcb5aa9276c1
abusayeds <abusayedstudent855@gmail.com>
2026-04-07 09:02:19 +0600

    app.ts file update

--- src/app.ts
+++ src/app.ts
@@ -29,6 +29,8 @@ app.use(
 );
 app.use(express.static("public"));
 app.use(logHttpRequests);
+app.use(express.json({ limit: "16mb" }));
+app.use(express.urlencoded({ limit: "16mb", extended: true }));
 // app.use((req: Request, res: Response, next: NextFunction) => {
 //   logger.info(`Incoming request: ${req.method} ${req.url}`);
 
