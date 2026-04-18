5ec2c9d9a6693c82d620852f79f3516a879e41da
abusayeds <abusayedstudent855@gmail.com>
2026-04-18 17:16:31 +0600

    invoice management modal create

---
+++ src/modules/make_modules/invoiceManagement/invoice.management.model.ts
@@ -0,0 +1,100 @@
+import { Schema, model, Types } from "mongoose";
+import { InvoiceManagementType, TInvoiceManagement } from "./invoice.management.interface";
+
+const addressSchema = new Schema(
+  {
+    street: { type: String, required: true },
+    street2: { type: String },
+    city: { type: String, required: true },
+    state: { type: String },
+    zip: { type: String },
+    country: { type: String, required: true },
+  },
+  { _id: false }
+);
+
+const productSchema = new Schema(
+  {
+    product_id: { type: Types.ObjectId, ref: "Product", required: true },
+    quantity: { type: Number, required: true },
+    rate: { type: Number, required: true },
+    tax: { type: Number, default: 0 },
+    discount: { type: Number, default: 0 },
+    amount: { type: Number, required: true },
+  },
+  { _id: false }
+);
+
+const serviceSchema = new Schema(
+  {
+    service_id: { type: Types.ObjectId, ref: "Service", },
+    quantity: { type: Number, },
+    rate: { type: Number, },
+    tax: { type: Number, default: 0 },
+    discount: { type: Number, default: 0 },
+    amount: { type: Number, },
+  },
+  { _id: false }
+);
+
+const invoiceManagementSchema = new Schema<TInvoiceManagement>(
+  {
+    type: {
+      type: String,
+      enum: Object.values(InvoiceManagementType),
+      required: true,
+    },
+
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    customer_id: {
+      type: Schema.Types.ObjectId,
+      ref: "Customer",
+    },
+
+    invoice_number: { type: String },
+
+    currency: { type: String, },
+
+    date: { type: Date, },
+    due_date: { type: Date, },
+
+    sub_title: { type: String },
+    po: { type: Schema.Types.Mixed },
+
+    shipping_method: { type: String },
+    payment_method: [{ type: String }],
+
+    discount_before_tax: { type: Number, default: 0 },
+
+    billing_address: { type: addressSchema,  },
+    shipping_address: { type: addressSchema,  },
+
+    product: [productSchema],
+    service: [serviceSchema],
+
+    terms_and_conditions: { type: String },
+    notes: { type: String },
+    internal_notes: { type: String },
+
+    Attachment: { type: String },
+
+    sub_total: { type: Number, default: 0 },
+    deposit: { type: Number, default: 0 },
+    discount: { type: Number, default: 0 },
+    shipping_cost: { type: Number, default: 0 },
+    total: { type: Number, default: 0 },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const InvoiceManagementModel = model<TInvoiceManagement>(
+  "InvoiceManagement",
+  invoiceManagementSchema
+);
