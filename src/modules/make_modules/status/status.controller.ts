218ced72073cada37051a56de9878cfc7baa4d77
abusayeds <abusayedstudent855@gmail.com>
2026-04-25 08:53:18 +0600

    graphChart make it in controller

--- src/modules/make_modules/status/status.controller.ts
+++ src/modules/make_modules/status/status.controller.ts
@@ -5,7 +5,8 @@ import sendResponse from "../../../utils/sendResponse";
 import { statusService } from "./status.service";
 
 const getStatusData = catchAsync(async (req: AuthRequest, res) => {
-  const result = await statusService.getStatusDataDB();
+  const {date} = req.params;
+  const result = await statusService.getStatusDataDB(req.user?._id as string , req.query , date);
   sendResponse(res, {
     success: true,
     statusCode: httpStatus.OK,
@@ -13,7 +14,37 @@ const getStatusData = catchAsync(async (req: AuthRequest, res) => {
     data: result,
   });
 });
+const graphChart = catchAsync(async (req: AuthRequest, res) => {
+  const result = await statusService.graphChartDB(req.user?._id as string , req.query );
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: " Graph Chart Data retrieved successfully.",
+    data: result,
+  });
+});
+const topCustomer = catchAsync(async (req: AuthRequest, res) => {
+  const result = await statusService.topCustomerDB(req.user?._id as string);
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: " Top Customer Data retrieved successfully.",
+    data: result,
+  });
+});
+const topProducts = catchAsync(async (req: AuthRequest, res) => {
+  const result = await statusService.topProductsDB(req.user?._id as string);
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: " Top Products Data retrieved successfully.",
+    data: result,
+  });
+});
 
 export const statusController =  {
-  getStatusData
+  getStatusData,
+  graphChart,
+  topCustomer, 
+  topProducts
 }
