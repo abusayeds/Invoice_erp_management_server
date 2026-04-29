7e08de128009eac187cda7a8c517804fc9b3e0cd
abusayeds <abusayedstudent855@gmail.com>
2026-04-29 17:09:03 +0600

    Activities create

--- src/modules/make_modules/pdf.setting/pdf.setting.controller.ts
+++ src/modules/make_modules/pdf.setting/pdf.setting.controller.ts
@@ -4,22 +4,29 @@ import { AuthRequest } from "../../../middlewares/auth";
 import catchAsync from "../../../utils/catchAsync";
 import sendResponse from "../../../utils/sendResponse";
 import { pdfSettingService } from "./pdf.setting.service";
+import { ActivitiesType } from "../activities/activities.interface";
+import { Types } from "mongoose";
+import { activitiesService } from "../activities/activities.service";
 
 
-const PdfSettingUpdate = catchAsync(async (req: AuthRequest, res: Response) => {
+const PdfSettingUpdate = catchAsync(async (req: AuthRequest, res) => {
   const { pdfType } = req.params;
-  const result = await pdfSettingService.PdfSettingUpdateDB(pdfType, req.body);
+  const result = await pdfSettingService.PdfSettingUpdateDB(pdfType, req.body, req?.user?._id as string);
   sendResponse(res, {
     success: true,
     statusCode: httpStatus.OK,
     message: "PDF setting updated successfully.",
     data: result,
   });
+    await activitiesService.activitiesCreateDB({
+      user_id: req?.user?._id as Types.ObjectId,
+      type: ActivitiesType.Created,
+      title: `PDF Setting Updated`,
+    });
 });
 
 
 
 export const pdfSettingController = {
-
   PdfSettingUpdate,
 };
