77713c2752452c947d02566cf8558a45eeaedf35
abusayeds <abusayedstudent855@gmail.com>
2026-05-02 11:48:30 +0600

    service

--- src/modules/make_modules/category/category.service.ts
+++ src/modules/make_modules/category/category.service.ts
@@ -1,7 +1,6 @@
 import { TCategory } from "./category.interface";
 import { CategoryModel } from "./category.model";
 
-
 // CREATE
 const createCategoryDB = async (payload: TCategory) => {
   const result = await CategoryModel.create(payload);
@@ -9,25 +8,35 @@ const createCategoryDB = async (payload: TCategory) => {
 };
 
 // GET ALL
-const getAllCategoryDB = async () => {
-  return await CategoryModel.find().sort({ createdAt: -1 });
+const getAllCategoryDB = async ( user_id :  string,category: string) => {
+  if (category) {
+    return await CategoryModel.find({type: category , user_id})
+      .sort({ createdAt: -1 });
+  } else {
+    return await CategoryModel.find({user_id})
+      .select("category")
+      .sort({ createdAt: -1 });
+  }
 };
 
 // GET SINGLE
-const getSingleCategoryDB = async (id: string) => {
-  return await CategoryModel.findById(id);
+const getSingleCategoryDB = async (id: string , user_id : string) => {
+  console.log(id);
+  console.log(user_id);
+  
+  return await CategoryModel.findOne({_id:id , user_id});
 };
 
 // UPDATE
-const updateCategoryDB = async (id: string, payload: Partial<TCategory>) => {
-  return await CategoryModel.findByIdAndUpdate(id, payload, {
+const updateCategoryDB = async (id: string, payload: Partial<TCategory> , user_id : string) => {
+  return await CategoryModel.findOneAndUpdate({_id:id , user_id}, payload, {
     new: true,
   });
 };
 
 // DELETE
-const deleteCategoryDB = async (id: string) => {
-  return await CategoryModel.findByIdAndDelete(id);
+const deleteCategoryDB = async (id: string , user_id : string) => {
+  return await CategoryModel.findOneAndDelete({_id : id  , user_id});
 };
 
 export const categoryService = {
