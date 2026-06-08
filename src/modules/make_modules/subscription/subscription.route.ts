caaa1ded1593bfcc14cdf08f624434e2e8ee1dba
abusayeds <abusayedstudent855@gmail.com>
2026-06-08 11:26:57 +0600

    feat(subscription): aggregate plan and checkout routes

--- src/modules/make_modules/subscription/subscription.route.ts
+++ src/modules/make_modules/subscription/subscription.route.ts
@@ -1,14 +1,11 @@
 import express from "express";
-import { subscriptionController } from "./subscription.controller";
-import { authMiddleware } from "../../../middlewares/auth";
-import { role } from "../../../utils/role";
- 
+import { planRoutes } from "./plan/plan.route";
+import { checkoutRoutes } from "./checkout/checkout.route";
+
 const router = express.Router();
- 
-router.post("/create", authMiddleware(role.superadmin),  subscriptionController.createSubscription);
-router.get("/", authMiddleware(role.superadmin , role.company) , subscriptionController.getAllSubscriptions);
-router.get("/:id", authMiddleware(role.superadmin , role.company) , subscriptionController.getSingleSubscription);
-router.patch("/:id", authMiddleware(role.superadmin) , subscriptionController.updateSubscription);
-router.delete("/:id",authMiddleware(role.superadmin), subscriptionController.deleteSubscription);
- 
+
+router.use("/plans", planRoutes);
+
+router.use("/", checkoutRoutes);
+
 export const subscriptionRoutes = router;
