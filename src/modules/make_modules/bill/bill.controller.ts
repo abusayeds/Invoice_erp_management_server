67de50988da59497e078c7dd830b06dbc0a3a7a1
abusayeds <abusayedstudent855@gmail.com>
2026-06-17 15:00:51 +0600

    feat(bill): restore the bill module and route
    Bring back the standalone bill module (model/interface/service/controller/
    route) and re-mount it at /api/v1/bill.

---
+++ src/modules/make_modules/bill/bill.controller.ts
@@ -0,0 +1,49 @@
+import httpStatus from 'http-status';
+import { AuthRequest } from '../../../middlewares/auth';
+import catchAsync from '../../../utils/catchAsync';
+import sendResponse from '../../../utils/sendResponse';
+import { billService } from './bill.service';
+import { Types } from 'mongoose';
+import { ActivitiesType } from '../activities/activities.interface';
+import { activitiesService } from '../activities/activities.service';
+import { TBill } from './bill.interface';
+
+const create = catchAsync(async (req: AuthRequest, res) => {
+  req.body.user_id = req?.user?._id;
+  const result: TBill = await billService.createDB(req.body);
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Bill created successfully.',
+    data: result,
+  });
+  await activitiesService.activitiesCreateDB({ 
+    user_id: req?.user?._id as Types.ObjectId, 
+    type: ActivitiesType.Created, 
+    title: 'Bill Create' 
+  });
+});
+
+const getSingle = catchAsync(async (req: AuthRequest, res) => {
+  const { id } = req.params;
+  const result = await billService.getSingleDB(id, req.user?._id as string);
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Bill retrieved successfully.',
+    data: result,
+  });
+});
+
+const getAll = catchAsync(async (req: AuthRequest, res) => {
+  const result = await billService.getAllDB(req.query, req.user?._id as string);
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Bills retrieved all successfully.',
+    data: result.allRecords,
+    pagination: result.pagination,
+  });
+});
+
+export const billController = { create, getSingle, getAll };
