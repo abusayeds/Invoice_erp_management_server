63a5ad749e21811b4e2c10ccdef0fdf3c2ec61c5
abusayeds <abusayedstudent855@gmail.com>
2026-06-25 11:49:21 +0600

    update(support/ticket-field): align routes

--- src/modules/make_modules/support/ticketField/ticketField.route.ts
+++ src/modules/make_modules/support/ticketField/ticketField.route.ts
@@ -1,15 +1,17 @@
 import express from "express";
 import { ticketFieldController } from "./ticketField.controller";
 import { authMiddleware } from "../../../../middlewares/auth";
+import { permissionMiddleware } from "../../../../middlewares/permissionMiddleware";
 import { role } from "../../../../utils/role";
+import { P } from "../shared/support.permissions";
 
 const router = express.Router();
-const auth = authMiddleware(role.company);
+const auth = authMiddleware(role.company, role.staff);
 
-router.post("/create", auth, ticketFieldController.create);
-router.get("/all", auth, ticketFieldController.getAll);
-router.get("/:id", auth, ticketFieldController.getSingle);
-router.patch("/:id", auth, ticketFieldController.update);
-router.delete("/:id", auth, ticketFieldController.remove);
+router.post("/create", auth, permissionMiddleware(P.ticket.edit_support_tickets), ticketFieldController.create);
+router.get("/all", auth, permissionMiddleware(P.ticket.manage_support_tickets), ticketFieldController.getAll);
+router.get("/:id", auth, permissionMiddleware(P.ticket.manage_support_tickets), ticketFieldController.getSingle);
+router.patch("/:id", auth, permissionMiddleware(P.ticket.edit_support_tickets), ticketFieldController.update);
+router.delete("/:id", auth, permissionMiddleware(P.ticket.edit_support_tickets), ticketFieldController.remove);
 
 export const ticketFieldRoutes = router;
