74cbde6e5f70bda4ca7561ba5b78865c69b0df64
abusayeds <abusayedstudent855@gmail.com>
2026-06-25 11:49:20 +0600

    update(support/ticket-category): align service

--- src/modules/make_modules/support/ticketCategory/ticketCategory.service.ts
+++ src/modules/make_modules/support/ticketCategory/ticketCategory.service.ts
@@ -1,12 +1,13 @@
 import { TTicketCategory } from "./ticketCategory.interface";
 import { TicketCategoryModel } from "./ticketCategory.model";
+import { createSupportCrudService } from "../shared/support.crud.service";
+import { P } from "../shared/support.permissions";
 
-const createDB = async (payload: TTicketCategory) => TicketCategoryModel.create(payload);
-const getAllDB = async (user_id: string) => TicketCategoryModel.find({ user_id, isDeleted: false }).sort({ createdAt: -1 });
-const getSingleDB = async (id: string, user_id: string) => TicketCategoryModel.findOne({ _id: id, user_id, isDeleted: false });
-const updateDB = async (id: string, payload: Partial<TTicketCategory>, user_id: string) =>
-  TicketCategoryModel.findOneAndUpdate({ _id: id, user_id, isDeleted: false }, payload, { new: true });
-const deleteDB = async (id: string, user_id: string) =>
-  TicketCategoryModel.findOneAndUpdate({ _id: id, user_id, isDeleted: false }, { isDeleted: true }, { new: true });
-
-export const ticketCategoryService = { createDB, getAllDB, getSingleDB, updateDB, deleteDB };
+export const ticketCategoryService = createSupportCrudService<TTicketCategory>({
+  model: TicketCategoryModel,
+  label: "Ticket category",
+  perms: { manageAny: P.category.manage_any_ticket_categories, manageOwn: P.category.manage_own_ticket_categories },
+  searchFields: ["name"],
+  nameField: "name",
+  formatItem: (d) => ({ _id: d._id, name: d.name, color: d.color ?? "#6B7280", createdAt: d.createdAt, updatedAt: d.updatedAt }),
+});
