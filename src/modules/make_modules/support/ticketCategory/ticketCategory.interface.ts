ec7729ad1a5fc3b1f99acff3ca219566204fe47e
abusayeds <abusayedstudent855@gmail.com>
2026-06-25 11:49:19 +0600

    update(support/ticket-category): align interface

--- src/modules/make_modules/support/ticketCategory/ticketCategory.interface.ts
+++ src/modules/make_modules/support/ticketCategory/ticketCategory.interface.ts
@@ -3,7 +3,10 @@ import { Types } from "mongoose";
 export interface TTicketCategory {
   _id?: string;
   user_id?: Types.ObjectId;
+  creator_id?: Types.ObjectId;
   name: string;
   color?: string;
   isDeleted?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
