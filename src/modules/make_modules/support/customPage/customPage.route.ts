c84ee9a1a8fefe5b506ba32bae1d15952757e172
abusayeds <abusayedstudent855@gmail.com>
2026-06-25 11:49:11 +0600

    update(support/custom-page): align custom page routes

--- src/modules/make_modules/support/customPage/customPage.route.ts
+++ src/modules/make_modules/support/customPage/customPage.route.ts
@@ -1,15 +1,17 @@
 import express from "express";
 import { customPageController } from "./customPage.controller";
 import { authMiddleware } from "../../../../middlewares/auth";
+import { permissionMiddleware } from "../../../../middlewares/permissionMiddleware";
 import { role } from "../../../../utils/role";
+import { P } from "../shared/support.permissions";
 
 const router = express.Router();
-const auth = authMiddleware(role.company);
+const auth = authMiddleware(role.company, role.staff);
 
-router.post("/create", auth, customPageController.create);
-router.get("/all", auth, customPageController.getAll);
-router.get("/:id", auth, customPageController.getSingle);
-router.patch("/:id", auth, customPageController.update);
-router.delete("/:id", auth, customPageController.remove);
+router.post("/create", auth, permissionMiddleware(P.customPage.create_support_ticket_custom_pages), customPageController.create);
+router.get("/all", auth, permissionMiddleware(P.customPage.manage_support_ticket_custom_pages), customPageController.getAll);
+router.get("/:id", auth, permissionMiddleware(P.customPage.manage_support_ticket_custom_pages), customPageController.getSingle);
+router.patch("/:id", auth, permissionMiddleware(P.customPage.edit_support_ticket_custom_pages), customPageController.update);
+router.delete("/:id", auth, permissionMiddleware(P.customPage.delete_support_ticket_custom_pages), customPageController.remove);
 
 export const customPageRoutes = router;
