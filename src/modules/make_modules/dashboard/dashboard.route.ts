12af740878d8e6e62ac1c292642c2dc6091349ad
abusayeds <abusayedstudent855@gmail.com>
2026-06-25 11:49:24 +0600

    chore(dashboard): mount CRM and support dashboard routes

--- src/modules/make_modules/dashboard/dashboard.route.ts
+++ src/modules/make_modules/dashboard/dashboard.route.ts
@@ -3,6 +3,8 @@ import { projectDashboardRoutes } from "./project/project.dashboard.route";
 import { accountDashboardRoutes } from "./account/account.dashboard.route";
 import { hrmDashboardRoutes } from "./hrm/hrm.dashboard.route";
 import { recruitmentDashboardRoutes } from "./recruitment/recruitment.dashboard.route";
+import { crmDashboardRoutes } from "./crm/crm.dashboard.route";
+import { supportDashboardRoutes } from "./support/support.dashboard.route";
 
 const router = express.Router();
 
@@ -11,5 +13,7 @@ router.use("/project", projectDashboardRoutes);
 router.use("/account", accountDashboardRoutes);
 router.use("/hrm", hrmDashboardRoutes);
 router.use("/recruitment", recruitmentDashboardRoutes);
+router.use("/crm", crmDashboardRoutes);
+router.use("/support", supportDashboardRoutes);
 
 export const dashboardRoutes = router;
