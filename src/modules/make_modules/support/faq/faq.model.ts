70d7c459f514069fc5c9a9863a0dc8338b8442c4
abusayeds <abusayedstudent855@gmail.com>
2026-06-25 11:49:12 +0600

    update(support/faq): align faq model with Laravel

--- src/modules/make_modules/support/faq/faq.model.ts
+++ src/modules/make_modules/support/faq/faq.model.ts
@@ -4,6 +4,7 @@ import { TFaq } from "./faq.interface";
 const faqSchema = new Schema<TFaq>(
   {
     user_id: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+    creator_id: { type: Schema.Types.ObjectId, ref: "User" },
     title: { type: String, required: true, trim: true },
     description: { type: String },
     isDeleted: { type: Boolean, default: false },
