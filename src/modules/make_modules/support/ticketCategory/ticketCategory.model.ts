e054d088fd1fc9554c7aec2eb594f66f2fa5fb4f
abusayeds <abusayedstudent855@gmail.com>
2026-06-25 11:49:19 +0600

    update(support/ticket-category): align model

--- src/modules/make_modules/support/ticketCategory/ticketCategory.model.ts
+++ src/modules/make_modules/support/ticketCategory/ticketCategory.model.ts
@@ -4,6 +4,7 @@ import { TTicketCategory } from "./ticketCategory.interface";
 const ticketCategorySchema = new Schema<TTicketCategory>(
   {
     user_id: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+    creator_id: { type: Schema.Types.ObjectId, ref: "User" },
     name: { type: String, required: true, trim: true },
     color: { type: String, default: "#000000" },
     isDeleted: { type: Boolean, default: false },
