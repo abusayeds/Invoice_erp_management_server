caeca19a0bcd880e695d902d04b31b6675cc0779
abusayeds <abusayedstudent855@gmail.com>
2026-06-25 11:49:12 +0600

    update(support/faq): align faq interface with Laravel

--- src/modules/make_modules/support/faq/faq.interface.ts
+++ src/modules/make_modules/support/faq/faq.interface.ts
@@ -3,7 +3,10 @@ import { Types } from "mongoose";
 export interface TFaq {
   _id?: string;
   user_id?: Types.ObjectId;
+  creator_id?: Types.ObjectId;
   title: string;
   description?: string;
   isDeleted?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
