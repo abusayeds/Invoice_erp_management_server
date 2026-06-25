1bbbfc81829d71d34622414e8068678131f2e7b0
abusayeds <abusayedstudent855@gmail.com>
2026-06-25 11:49:13 +0600

    update(support/knowledge): align knowledge model

--- src/modules/make_modules/support/knowledge/knowledge.model.ts
+++ src/modules/make_modules/support/knowledge/knowledge.model.ts
@@ -4,6 +4,7 @@ import { TKnowledge } from "./knowledge.interface";
 const knowledgeSchema = new Schema<TKnowledge>(
   {
     user_id: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+    creator_id: { type: Schema.Types.ObjectId, ref: "User" },
     title: { type: String, required: true, trim: true },
     description: { type: String },
     category: { type: Schema.Types.ObjectId, ref: "SupportKnowledgeCategory" },
